74a103130d4789d71da094dad5c8ab482d17448e
Ruben Van Assche <rubenvanassche@gmail.com>
2024-06-21 17:04:41 +0200

    Fix issue where laravel could not be optimized anymore

--- src/LaravelErrorShareServiceProvider.php
+++ src/LaravelErrorShareServiceProvider.php
@@ -10,14 +10,26 @@ class LaravelErrorShareServiceProvider extends PackageServiceProvider
 {
     public function registeringPackage(): void
     {
-        View::prependNamespace('laravel-exceptions-renderer', [__DIR__.'/../resources/views']);
+        if ($this->canIncludeViews()) {
+            View::prependNamespace('laravel-exceptions-renderer', [__DIR__.'/../resources/views']);
+        }
     }
 
     public function configurePackage(Package $package): void
     {
         $package
             ->name('laravel-error-share')
-            ->hasConfigFile()
-            ->hasViews();
+            ->hasConfigFile();
+
+
+        if ($this->canIncludeViews()) {
+            $package->hasViews();
+        }
+    }
+
+    protected function canIncludeViews(): bool
+    {
+        // Otherwise php artisan optimize may crash
+        return config('app.debug') === true;
     }
 }
